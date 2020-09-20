feb5766ae5ef6ab2aec308ae12a4f781ca46568f
Gustavo Ferreira <gu.u@hotmail.com>
2020-09-20 11:13:13 -0300

    teste de remocao

--- src/test/java/br/ce/wcaquino/tasks/apitest/APItest.java
+++ src/test/java/br/ce/wcaquino/tasks/apitest/APItest.java
@@ -48,6 +48,32 @@ public class APItest {
 			.body("message", CoreMatchers.is("Due date must not be in past"))
 		;
 	}
+	
+	@Test
+	public void deveRemoverTarefaComSucesso() {
+		//inserir
+		Integer id = RestAssured.given()
+			.body("{ \"task\": \"Tarefa para remocao\", \"dueDate\": \"2020-12-30\" }")
+			.contentType(ContentType.JSON)
+			.when()
+			.post("/todo")
+			.then()
+//			.log().all()
+			.statusCode(201)
+			.extract().path("id")
+	;
+		System.out.println(id);
+		//remover
+		RestAssured.given()
+		.when()
+			.delete("/todo/"+id)
+		.then()
+			.statusCode(204)
+		;
+		
+		
+	}
+
 }
 
 
